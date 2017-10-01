7b1368e58e42fdd711ab54a9f7774ccb27186370
tvlangley <chibia2@gmail.com>
2017-10-01 19:32:42 -0400

    test

--- src/components/layout/Body.js
+++ src/components/layout/Body.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import ParkList from '../park/ParkList';
 import MyParkList from '../park/MyParkList';
+import * as firebase from 'firebase';
+import * as reactfire from 'reactfire';
+
+// var firebase = require('firebase');
+const config = {
+  apiKey: "AIzaSyA428dlWWw9ExOTapCiN7jGAOlqPf4T-_s",
+  authDomain: "parkfinderdatabase.firebaseapp.com",
+  databaseURL: "https://parkfinderdatabase.firebaseio.com",
+  projectId: "parkfinderdatabase",
+  storageBucket: "",
+  messagingSenderId: "720724333567"
+};
+firebase.initializeApp(config);
 
 class Body extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      parks: [
-        {name:'State Park 1', hasVisited: false, location: 'Maryland', showInfo: false},
-        {name:'State Park 2', hasVisited: false, location: 'Pennsylvania', showInfo: false},
-        {name:'State Park 3', hasVisited: false, location: 'Massachusetts', showInfo: false}
-      ],
+      parks: [],
+      // parks: [
+      //   {name:'State Park 1', hasVisited: false, location: 'Maryland', showInfo: false},
+      //   {name:'State Park 2', hasVisited: false, location: 'Pennsylvania', showInfo: false},
+      //   {name:'State Park 3', hasVisited: false, location: 'Massachusetts', showInfo: false}
+      // ],
       myParks: []
     }
     this.handleParkClick = this.handleParkClick.bind(this)
@@ -18,8 +32,34 @@ class Body extends Component {
     this.handleParkItemClick = this.handleParkItemClick.bind(this)
     this.handleParkItemDelete = this.handleParkItemDelete.bind(this)
   }
+  mixins: [ReactFireMixin]
 
-  handleParkClick (index) {
+  componentWillMount() {
+    const ref = firebase.database().ref("parks");
+    ref.on('value', function(snapshot){
+      // const parks = []
+      // console.log(snapshot.val())
+      const parks = snapshot.val()
+      // park['.key'] = snapshot.key;
+      // parks.push(park)
+      console.log(parks)
+      this.setState({
+        parks: parks
+      });
+    }).bind(this)
+    console.log(this.state.parks)
+    // this.firebaseRef = firebase.database().ref("parks");
+    // console.log("firebaseRef: ", this.firebaseRef)
+    // this.firebaseRef.on("child_added", function(dataSnapshot) {
+    //   this.parks.push(dataSnapshot.val());
+    //   console.log("parks is: ", this.parks)
+      // this.setState({
+      //   parks: this.parks
+      // });
+    // }.bind(this));
+  }
+
+  handleParkClick(index) {
     if(this.state.myParks.indexOf(this.state.myParks[index]) === -1) {
       this.setState({
         myParks: this.state.myParks.concat([this.state.parks[index]])
