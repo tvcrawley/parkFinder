9ec4943b7b4e895fc5293359e734d0d23e3c5244
tvlangley <chibia2@gmail.com>
2017-09-30 20:14:22 -0400

    Create layout components
    Create components for the three main sections of the site

---
+++ src/components/layout/Body.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+  render() {
+    return (
+      <div className="Body">
+        <div>Here's some content</div>
+      </div>
+    );
+  }
+}
+
+export default Body;
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        <h5>Made by Toni Langley</h5>
+      </div>
+    );
+  }
+}
+
+export default Footer;
