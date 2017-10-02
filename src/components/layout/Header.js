15703ae322c51eaa6b400b83d2f78808a499d15c
tvlangley <chibia2@gmail.com>
2017-10-01 22:30:27 -0400

    Add app instruction
    Add app instruction for user

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,6 +4,7 @@ function Header(props) {
   return (
     <div className="Header">
       <h1>Welcome to Park Finder!</h1>
+      <h4>Click on a park to add it to your list.</h4>
     </div>
   );
 }
