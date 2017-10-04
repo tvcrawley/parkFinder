be2bcd70449a98dac78ebf353e099d56ff38f2bf
tvlangley <chibia2@gmail.com>
2017-10-03 23:39:08 -0400

    Change park name header
    Change park name header from `h5` to `h4`

--- src/components/park/ParkList.js
+++ src/components/park/ParkList.js
@@ -4,9 +4,9 @@ import './ParkList.css';
 function ParkList(props) {
   return (
     <div className="ParkList">
-      <h5 onClick={() => props.onParkClick(0)}>{props.parks[0].name}</h5>
-      <h5 onClick={() => props.onParkClick(1)}>{props.parks[1].name}</h5>
-      <h5 onClick={() => props.onParkClick(2)}>{props.parks[2].name}</h5>
+      <h4 onClick={() => props.onParkClick(0)}>{props.parks[0].name}</h4>
+      <h4 onClick={() => props.onParkClick(1)}>{props.parks[1].name}</h4>
+      <h4 onClick={() => props.onParkClick(2)}>{props.parks[2].name}</h4>
     </div>
   );
 }
